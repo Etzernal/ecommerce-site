d1c97a0842e1fdd3d2cc02931830170eabc6d447
Etzernal <chan.jielin@dhs.sg>
2020-08-20 22:12:08 +0800

    Creates react app

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# ecommerce-site
+
+Following tutorial from https://www.youtube.com/watch?v=1_IYL9ZMR_Y
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
